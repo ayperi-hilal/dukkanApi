f65ecf21a468acb7533ac8df89fc21d5e45497df
ayperi-hilal <sa.narmanhilal_2000@hotmail.com>
2022-09-02 17:49:02 +0300

    config kurulumu yapılacak

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native'; /**olmaz ise sayfa geçişleri sağlanamaz. */
+import {createStackNavigator} from '@react-navigation/stack'/**sayfadan sayfaya geçiş yapmak için */
+import Products from './src/pages/Products/Products';
+import ProductsDeatils from './src/pages/Products/ProductsDeatils';
+
+
+
+const Stack=createStackNavigator()
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name='ProductPage' component={Products}/>
+        <Stack.Screen name='DetailPga' component={ProductsDeatils}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ src/pages/Products/ProductsDeatils.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {View,Text} from 'react-native'
+
+const ProductsDeatils =()=>{
+    return(
+        <View>
+            <Text>
+            ProductsDeatils
+            </Text>
+        </View>
+    );
+};
+
+export default ProductsDeatils;
